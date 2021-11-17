44e336af610df02aab0a1d09e2962304d859bf6a
Gustavo <gar.santeliz@gmail.com>
2021-11-17 11:40:34 -0500

    start course from scratch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div>Hello!</div>
+  return <div>
+    <h1>My To-Dos</h1>
+    <div className="card">
+      <h3>My Title</h3>
+      <button>Delete</button>
+    </div>
+  </div>
 }
 
 export default App;
